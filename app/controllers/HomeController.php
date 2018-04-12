4095b1fd8c7d01df630a7b86edfd2410acef98d7
Jeffrey Pogoy <jypogoy@gmail.com>
2018-04-12 15:05:51 +0800

    Added logger call.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,5 +1,8 @@
 <?php
 
+use Phalcon\Logger;
+use Phalcon\Logger\Adapter\File as FileAdapter;
+
 class HomeController extends ControllerBase
 {
 
@@ -13,6 +16,13 @@ class HomeController extends ControllerBase
     {
         $this->view->user = $this->session->get('auth');       
         var_dump($this->sessionLogger);
+
+        $logger = new FileAdapter(BASE_PATH . '/logs/test.log');
+
+        $logger->critical(
+            'This is a critical message'
+        );
+
         $this->sessionLogger->info($this->session->get('auth')['name'] . ' @ Home page.'); 
     }
 
