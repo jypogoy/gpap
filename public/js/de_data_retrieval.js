a9f384c77cee3d32c61f1f921dda5026a4accfaf
Jeffrey Pogoy <jeffrey.pogoy@amdatex.com>
2018-08-03 15:30:08 +0800

    Fix for missing ruler.

--- public/js/de_data_retrieval.js
+++ public/js/de_data_retrieval.js
@@ -415,7 +415,8 @@ function getRegionCurrency() {
             if (merchantInfoMap.get('acceptOtherCurrency') == 'Y') {
                 $('<div class="item" data-value="34">Other</div>').appendTo(menuWrapper);              
             }            
-            $('.dropdown.icon').empty(); // Remove extra dropdown shown upon populating items.
+            console.log($('.dropdown.icon:not(#rulerIcon)'));
+            $('i.dropdown.icon').empty(); // Remove extra dropdown shown upon populating items.
         }   
         d.resolve(data);             
     })
